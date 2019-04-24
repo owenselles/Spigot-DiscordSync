afc18e13a6e318709ac85af5ea46b6cbdc0b9abd
highbanana <47282593+highbanana@users.noreply.github.com>
2019-04-24 23:38:24 +0200

    Placeholders and messages

--- DiscordSync/spigot/net/evolvedmc/discordsync/discord/Discord.java
+++ DiscordSync/spigot/net/evolvedmc/discordsync/discord/Discord.java
@@ -43,9 +43,9 @@ public class Discord extends ListenerAdapter implements CommandExecutor, Listene
         uuidCodeMap = new HashMap<UUID, String>();
         uuidIdMap = new HashMap<UUID, String>();
         verifiedmembers = new ArrayList<UUID>();
-        jda.addEventListener(new Object[] { this });
+        jda.addEventListener(this);
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        plugin.getCommand("verify").setExecutor((CommandExecutor)this);
+        plugin.getCommand("verify").setExecutor(this);
         guild = jda.getGuilds().get(0);
     }
     
@@ -64,24 +64,33 @@ public class Discord extends ListenerAdapter implements CommandExecutor, Listene
             return;
         }
         String[] args = e.getMessage().getContentRaw().split(" ");
-        if (args[0].equalsIgnoreCase("!link")) {
-            if (e.getMember().getRoles().stream().filter(role -> role.getName().equals("123")).findAny().orElse(null) != null) {
-                e.getChannel().sendMessage((":x: Error!" + e.getAuthor().getAsMention() + ", you are already verified!")).queue();
+        if (args[0].equalsIgnoreCase(plugin.getConfig().getString("Command.Discord"))) {
+            if (e.getMember().getRoles().stream().filter(role -> role.getName().equals(plugin.getConfig().getString("Role-name"))).findAny().orElse(null) != null) {
+                e.getChannel().sendMessage(plugin.getConfig().getString(plugin.getConfig().getString("Messages.Discord.Already-Verified")
+                		.replaceAll("%dname%", e.getAuthor().getName()
+                		.replaceAll("%dtag%", e.getAuthor().getDiscriminator())))).queue();
                 return;
             }
             if (args.length != 2) {
-                e.getChannel().sendMessage(":x: Error! You need to specify a player!").queue();
+                e.getChannel().sendMessage(plugin.getConfig().getString("Messages.Discord.Specify-Player")
+                		.replaceAll("%dname%", e.getAuthor().getName())
+                        .replaceAll("%dtag%", e.getAuthor().getDiscriminator())).queue();
                 return;
             }
             Player target = Bukkit.getPlayer(args[1]);
             if (target == null) {
-                e.getChannel().sendMessage(":x: Error! The player needs to be online!").queue();
+                e.getChannel().sendMessage(plugin.getConfig().getString("Messages.Discord.Offline-Player")
+                		.replaceAll("%dname%", e.getAuthor().getName())
+                		.replaceAll("%dtag%", e.getAuthor().getDiscriminator())).queue();
                 return;
             }
             String verifycode = String.valueOf(new Random().nextInt(800000) + 200000) + "EV";
             uuidCodeMap.put(target.getUniqueId(), verifycode);
             uuidIdMap.put(target.getUniqueId(), e.getAuthor().getId());
-            e.getAuthor().openPrivateChannel().complete().sendMessage(("Your verification code has been generated!\n Execute this command in game to verify your account: /verify " + verifycode)).queue();
+            e.getAuthor().openPrivateChannel().complete().sendMessage(plugin.getConfig().getString("Messages.Discord.Generated-Key")
+            		.replaceAll("%dname%", e.getAuthor().getName()
+            		.replaceAll("%dtag%", e.getAuthor().getDiscriminator())
+            		.replaceAll("%key%", verifycode))).queue();
         }
     }
     
@@ -98,28 +107,29 @@ public class Discord extends ListenerAdapter implements CommandExecutor, Listene
             verifiedmembers.remove(e.getPlayer().getUniqueId());
         }
     }
-    
+   
+    @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can execute this command!");
+            sender.sendMessage(plugin.getConfig().getString("Messages.Minecraft.No-Console"));
             return true;
         }
         Player p = (Player)sender;
         if (plugin.getConfig().contains("Data." + p.getUniqueId().toString())) {
-            p.sendMessage("Error! You are already verified!");
+            p.sendMessage(plugin.getConfig().getString("Messages.Minecraft.Already-Verified"));
             return true;
         }
         if (!uuidCodeMap.containsKey(p.getUniqueId())) {
-            p.sendMessage("Not pending verification process!");
+            p.sendMessage(plugin.getConfig().getString("Messages.Minecraft.No-Pending"));
             return true;
         }
         if (args.length != 1) {
-            p.sendMessage("Usage: /verify [code]");
+            p.sendMessage(plugin.getConfig().getString("Messages.Minecraft.Usage"));
             return true;
         }
         String verify2code = uuidCodeMap.get(p.getUniqueId());
         if (!verify2code.equals(args[0])) {
-            p.sendMessage("Code is invalid! Please try again!");
+            p.sendMessage(plugin.getConfig().getString("Messages.Minecraft.Invalid-Key"));
             return true;
         }
         String discordid = uuidIdMap.get(p.getUniqueId());
@@ -127,7 +137,7 @@ public class Discord extends ListenerAdapter implements CommandExecutor, Listene
         if (target == null) {
             uuidCodeMap.remove(p.getUniqueId());
             uuidIdMap.remove(p.getUniqueId());
-            p.sendMessage(":x: Error! It seems that you are not connected to the discord server!");
+            p.sendMessage(plugin.getConfig().getString("Messages.Minecraft.Not-Connected"));
             return true;
         }
         Role verifiedrole = guild.getRolesByName(plugin.getConfig().getString("Role-name"), true).get(0);
@@ -137,9 +147,8 @@ public class Discord extends ListenerAdapter implements CommandExecutor, Listene
         uuidIdMap.remove(p.getUniqueId());
         plugin.getConfig().set("Data." + p.getUniqueId(), (Object)discordid);
         plugin.saveConfig();
-        target.getUser().openPrivateChannel().complete().sendMessage((":white_check_mark: Verified! you have been succesfully linked with your minecraft account (" + p.getName() + ")!"));
-        p.sendMessage("Verified! you have been succesfully linked with your discord account (" + target.getUser().getName() + "#" + target.getUser().getDiscriminator() + ")!");
-        p.sendMessage("You've been verified succesfully :)");
+        target.getUser().openPrivateChannel().complete().sendMessage(plugin.getConfig().getString("Messages.Discord.Verified-Succesfully")).queue();;
+        p.sendMessage(plugin.getConfig().getString("Messages.Minecraft.Verified-Succesfully"));
         return true;
     }
 }
